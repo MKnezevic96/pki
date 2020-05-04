c60a21f4e7e1275c28a3c542c34aff9b056a4be9
mitosHermanos <mitadima@gmail.com>
2020-05-04 13:21:28 +0200

    templates finished

--- src/main/resources/static/js/createRoot.js
+++ src/main/resources/static/js/createRoot.js
@@ -47,15 +47,7 @@ $(document).ready(function() {
 */
 
 
-    	//	e.preventDefault()
-
-
-
-
-
-
-
-
+    		e.preventDefault();
             let commonName = $('#commonName').val()
     		let localityName = $('#localityName').val()
     		let stateName = $('#stateName').val()
@@ -68,6 +60,86 @@ $(document).ready(function() {
     		let alias = $('#alias').val()
             let isCA = false;
 
+	 //-----------------------------------------------------------------
+	 let keyUsage = false;
+	 let digitalSignature = false;
+	 let keyEncipherment = false;
+	 let keyAgreement = false;
+	 let nonRepudiation = false;
+	 let extendedKeyUsage = false;
+	 let serverAuth = false;
+	 let clientAuth = false;
+	 let codeSigning = false;
+	 let emailProtection = false;
+	 let timeStamping = false;
+	 let ocspSigning = false;
+	 let keyCertSign = false;
+
+	 if($('#server_auth').is(":checked")){
+		 console.log('server');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 keyEncipherment = true;
+		 keyAgreement = true;
+		 extendedKeyUsage = true;
+		 serverAuth = true;
+	 }
+	 if($('#client_auth').is(":checked")){
+		 console.log('client');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 keyAgreement = true;
+		 extendedKeyUsage = true;
+		 clientAuth = true;
+	 }
+	 if($('#code_signing').is(":checked")){
+		 console.log('code');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 extendedKeyUsage = true;
+		 codeSigning = true;
+	 }
+	 if($('#email_protection').is(":checked")){
+		 console.log('email');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 keyEncipherment = true;
+		 nonRepudiation = true;
+		 extendedKeyUsage = true;
+		 emailProtection = true;
+	 }
+	 if($('#time_stamping').is(":checked")){
+		 console.log('time');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 nonRepudiation = true;
+		 extendedKeyUsage = true;
+		 timeStamping = true;
+	 }
+	 if($('#OCSP_signing').is(":checked")){
+		 console.log('ocsp');
+		 keyUsage = true;
+		 digitalSignature = true;
+		 nonRepudiation = true;
+		 extendedKeyUsage = true;
+		 ocspSigning = true;
+	 }
+	 if(isCA){
+		 keyUsage = true;
+		 keyCertSign = true;
+		 extendedKeyUsage = true;
+		 ocspSigning = true;
+	 }
+	 let keyUsageDTO;
+	 let extendedKeyUsageDTO;
+	 keyUsageDTO = { "keyUsage": keyUsage, "digitalSignature": digitalSignature, "keyEncipherment": keyEncipherment, "keyAgreement": keyAgreement,  "nonRepudiation": nonRepudiation, "keyCertSign": keyCertSign}
+	 extendedKeyUsageDTO = { "extendedKeyUsage": extendedKeyUsage, "serverAuth": serverAuth, "clientAuth": clientAuth, "codeSigning": codeSigning, "emailProtection": emailProtection, "timeStamping": timeStamping, "ocspSigning": ocspSigning }
+	 console.log(keyUsageDTO);
+	 console.log(extendedKeyUsageDTO);
+
+	 //-----------------------------------------------------------------
+
+
     		let issuerDataDTO = {"commonName":commonName,"localityName":localityName,"stateName":stateName,
     		"countryName":countryName,"organisationName":organisationName, "organisationUnitName":organisationUnitName,"givenName":"",
     		"surname":"", "uid":"", "serialNumber":"","email":email}
@@ -76,10 +148,10 @@ $(document).ready(function() {
                                      		"countryName":countryName,"organisationName":organisationName, "organisationUnitName":organisationUnitName,"givenName":"",
                                      		"surname":"", "uid":"", "serialNumber":"","email":email}
 
-            let certDto = JSON.stringify({"issuerData":issuerDataDTO, "subjectData":subjectDataDTO, "keyStorePassword":keyStorePassword, "keyPassword": keyPassword,
-                                            "basicConstrains":isCA, "alias":alias})
+	 		let certDto = JSON.stringify({"issuerData":issuerDataDTO, "subjectData":subjectDataDTO, "keyStorePassword":keyStorePassword, "keyPassword": keyPassword,
+		 									"basicConstrains":isCA, "extendedKeyUsageDTO":extendedKeyUsageDTO, "keyUsageDTO":keyUsageDTO, "alias":alias})
 
-    		console.log(certDto)     // samo provera
+	 		console.log(certDto)     // samo provera
 
 
 
