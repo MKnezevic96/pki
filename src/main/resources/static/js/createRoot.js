d35cf62bfbea240c7a5ebff708bcd7b01351ee80
antonelaparovic <57138968+antonelaparovic@users.noreply.github.com>
2020-05-06 22:54:57 +0200

    root valid. alert

--- src/main/resources/static/js/createRoot.js
+++ src/main/resources/static/js/createRoot.js
@@ -2,71 +2,36 @@ $(document).ready(function() {
 
 
  $('#btnCreate').click(function(e){
-/*
- $("#contact").validate({
-    rules: {
-
-    commonName: {
-            required: true,
-            lettersonly: true
-          },
-    countryName: {
-             required: true,
-             lettersonly: true
-           },
-    stateName: {
-             required: true,
-             lettersonly: true
-           },
-    localityName: {
-                  required: true,
-                  lettersonly: true
-                },
-    organisationName: {
-                   required: true,
-                   lettersonly: true
-                 },
-    organisationUnitName: {
-                    required: true,
-                    lettersonly: true
-                  },
-    email: {
-        required: true,
-        email: true,
-        //remote: "http://localhost:3000/inputValidator"
-      },
-
-    alias: {
-        required: true,
-        nowhitespace: true,
-      }
-   }
-  })
 
 
-*/
-
-
-    	//	e.preventDefault()
+    		e.preventDefault()
+    	 let commonName = $('#commonName').val()
+            		let localityName = $('#localityName').val()
+            		let stateName = $('#stateName').val()
+            		let countryName = $('#countryName').val()
+            		let organisationName = $('#organisationName').val()
+            		let organisationUnitName = $('#organisationUnitName').val()
+            		let email = $('#email').val()
+            		let keyStorePassword = $('#keyStorePassword').val()
+            		let keyPassword = $('#keyPassword').val()
+            		let alias = $('#alias').val()
+                    let isCA = false;
 
 
 
+            var empty = false;
+            $('#contact input').each(function() {
+                if ($(this).val() == '') {
+                    empty = true;
+                }
+            });
+
+            if (empty) {
+                alert("Please fill empty fields.");
+            }
+            else {
 
 
-
-
-
-            let commonName = $('#commonName').val()
-    		let localityName = $('#localityName').val()
-    		let stateName = $('#stateName').val()
-    		let countryName = $('#countryName').val()
-    		let organisationName = $('#organisationName').val()
-    		let organisationUnitName = $('#organisationUnitName').val()
-    		let email = $('#email').val()
-    		let keyStorePassword = $('#keyStorePassword').val()
-    		let keyPassword = $('#keyPassword').val()
-    		let alias = $('#alias').val()
-            let isCA = false;
 
     		let issuerDataDTO = {"commonName":commonName,"localityName":localityName,"stateName":stateName,
     		"countryName":countryName,"organisationName":organisationName, "organisationUnitName":organisationUnitName,"givenName":"",
@@ -80,11 +45,6 @@ $(document).ready(function() {
                                             "basicConstrains":isCA, "alias":alias})
 
     		console.log(certDto)     // samo provera
-
-
-
-
-
 
     		$.ajax({
     			type: 'POST',
@@ -106,10 +66,14 @@ $(document).ready(function() {
                                         alert('Cannot make root certificate. ')
                                    }
                             }
-                   })
+
+                   	 });
+                   }    // else
 
 
+     //       })
     	})
+
 })
 
 
